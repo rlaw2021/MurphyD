19f1ddfb47f1e7158d9befac819316df35686ca2
donmurphycs <don.murphy.cs@gmail.com>
2018-01-23 11:23:09 -0800

    Cleaned up duplicate examples

--- Examples/CountToTen.java
+++
@@ -1,9 +0,0 @@
-// CountToTen.java
-
-public class CountToTen {
-    public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.print(i + " ");
-        }
-    }
-}
--- Examples/HelloYou.java
+++
@@ -1,7 +0,0 @@
-// HelloYou.java
-
-public class HelloYou {
-    public static void main(String[] args) {
-        System.out.println("Hello "+ args[0] + "!"); // Prints out a greeting, including the first argument string
-    }
-}
